2aeaee7edc8f53e830b5bb9abf299a5941a12efa
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-03-31 18:16:02 +0900

    userモデルのバリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :email,              presence: true #必要かメンターさんに聞く
+  validates :encrypted_password, presence: true #必要かメンターさんに聞く
+  validates :last_name,          presence: true
+  validates :first_name,         presence: true
+  validates :last_name_kana,     presence: true
+  validates :first_name_kana,    presence: true
+  validates :birthday,           presence: true
+  
 end
