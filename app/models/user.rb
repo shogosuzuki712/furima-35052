30ebc74075f9853b99f41086742a2ed6b913f7c4
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-03-31 23:51:03 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :email,              presence: true #必要かメンターさんに聞く
-  validates :encrypted_password, presence: true #必要かメンターさんに聞く
   validates :last_name,          presence: true
   validates :first_name,         presence: true
   validates :last_name_kana,     presence: true
