e07697506f2f86a7a53b8b2783514f551b9f1771
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-03 23:01:14 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :text, :price, :image).merge(user_id: current_user.id)
+  end
 end
