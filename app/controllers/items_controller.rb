04ed81eea433174c78296a49e4ac0aee87efb637
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-04 21:14:22 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,15 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(
-      :image, :title, :text, 
-      :category_id, state_id:, :fee_burden_id,
-      :prefecture_id, :ship_date_id, :price,
+      :image,
+      :title,
+      :text, 
+      :category_id,
+      :state_id,
+      :fee_burden_id,
+      :prefecture_id,
+      :ship_date_id,
+      :price
     ).merge(user_id: current_user.id)
   end
 end
