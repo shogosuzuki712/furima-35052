9398411bc025a10d528abb35fe2c4dc2dfd2afba
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-05 16:17:26 +0900

    販売手数料/利益表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price");
+  
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+    const addprofitDom = document.getElementById("profit");
+    addprofitDom.innerHTML = Math.floor(inputValue*0.9);
+  })
+  
+})
